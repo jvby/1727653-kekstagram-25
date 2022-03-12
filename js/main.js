aa56e01248c8df3ce9c757aa9edf65a24d57ad1a
jvby <belyaikinv@gmail.com>
2022-03-12 23:56:05 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
-function getRandomIntInclusive(min, max) {
-  if (min >= max || min < 0) {
-    return 'Unexpected data!';
-  } else {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
 
-function stringLength(stringCheck, maxLength) {
+function getStringLength(stringCheck, maxLength) {
   return  stringCheck.length <= maxLength;
 }
-stringLength('Привет',5);
-getRandomIntInclusive(40,90);
+getStringLength('Привет',5);
+getRandomPositiveInteger(40,90);
