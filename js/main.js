cf278e39dd7de575a4298c48c78b96b5e8c93ff0
jvby <belyaikinv@gmail.com>
2022-03-14 12:28:56 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
-function getRandomPositiveInteger(a, b) {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
-}
-
-function stringLength(stringCheck, maxLength) {
-  return  stringCheck.length <= maxLength;
-}
-stringLength('Привет',5);
-
-const NAME = [
+const NAMES = [
   'Иван',
   'Хуан Себастьян',
   'Мария',
@@ -20,7 +9,7 @@ const NAME = [
   'Вашингтон',
 ];
 
-const COMMENT = [
+const COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -28,7 +17,7 @@ const COMMENT = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
-const DESCRIPTION = [
+const DESCRIPTIONS = [
   'Гуляю',
   'На море',
   'Сижу дома',
@@ -38,33 +27,59 @@ const DESCRIPTION = [
   'С новой прической',
 ];
 
+const PHOTO_ARRAY_COUNT = 25;
+
+const START_ID = 0;
+
+function getRandomPositiveInteger(a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+}
+
+function checkStringLength(stringCheck, maxLength) {
+  return  stringCheck.length <= maxLength;
+}
+
+checkStringLength('Привет',5);
+
 const getRandomArrayElement = (elements) => {
   const element = elements[getRandomPositiveInteger(0, elements.length - 1)];
   return element;
 };
 
-const createComment = (idNumber) => {
+const createComment = (id) => {
   const comment = {
-    id: idNumber,
+    id: id,
     avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
-    message: getRandomArrayElement(COMMENT),
-    name: getRandomArrayElement(NAME),
+    message: getRandomArrayElement(COMMENTS),
+    name: getRandomArrayElement(NAMES),
   };
   return comment;
 };
 
-const createPhoto = (idNumber) => {
+function createCommentsArray(count, id) {
+  const comments = Array.from({length: count}, () => {
+    id++;
+    return createComment(id);
+  });
+  return comments;
+}
+
+const createPhoto = (id) => {
   const photo = {
-    id: idNumber,
-    url: `photos/${idNumber}.jpg`,
-    description: getRandomArrayElement(DESCRIPTION),
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: getRandomArrayElement(DESCRIPTIONS),
     likes: getRandomPositiveInteger(15, 200),
-    comments: Array.from({length: getRandomPositiveInteger(1, 10)}, (v,i) => createComment(i+1)),
+    comments: createCommentsArray(getRandomPositiveInteger(1, 10), START_ID),
   };
   return photo;
 };
 
-const photoArrayCount = 25;
+const createPhotoArray = (count, id) => Array.from({length: count}, () => {
+  id++;
+  return createPhoto(id);
+});
 
-const photoArray = () => Array.from({length: photoArrayCount}, (v,i) => createPhoto(i+1));
-photoArray();
+createPhotoArray(PHOTO_ARRAY_COUNT, START_ID);
